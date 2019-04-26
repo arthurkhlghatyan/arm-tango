4cf16d03ee8defb97d1dff5d28f1148c72c18878
Arthur Khlghatyan <arthur.khlghatyan@gmail.com>
2019-04-26 10:32:21 +0400

    Armenia tour text fixes

--- src/data/biographies/armenia-tour.js
+++ src/data/biographies/armenia-tour.js
@@ -1,11 +1,26 @@
 export default `
   <p>
-    “Armenia Tour” travel agency was founded in 2015 and spares no effort for developing its business intensively.
+    Armenia Tour  was founded in Yerevan in 2015 and during the 4 years of painstaking activities and detailed researches we have managed to become a solid and trusted participant of Armenian tourism and real estate services market.
   </p>
   <p>
-    The company is specialized in income tour and organizes various tours through Armenia, Nagorno Karabakh and Georgia not only for Armenians spread throughout the world, but also for foreign travelers who want to discover the culture of the oldest nationalities of the world, to see the unique beauty of Armenian nature.
+    Tour agency  has launched collaboration with many oversea companies and currently have representatives in Canada, The USA, Russia, Germany, Greece, Iran, Syria and Kuwait.
   </p>
   <p>
-    Our website is full of information related to the interesting places, hotels, rental apartments and car rent as well.
+    Company handles travel deals to Armenia, Artsakh, Javakhk, Western Armenia and Cilicia.  
+  </p>
+  <p>
+    Armenia Tour owns more then 70 rental apartments in the center of Yerevan and  cooperates with hotels in Yerevan, regions and in Artsakh.  
+  </p>
+  <p>
+    In our website http://armenia-tour.am/ and facebook page https://www.facebook.com/Armeniatouragency/ you will find individual, group and corporate tours with specially developed and multiply realized programs. The variety of trips is as diverse as our country itself – from classical sightseeing tours to food and wine tours as well as guided backpacking trips, hiking and trekking in mountains. 
+  </p>
+  <p>
+    With Armenia Tour everyone will feel the real “taste and smell” of sunny Armenia, its color, its favorable climate and its unique nature. You will have the opportunity to taste saint Armenian bread - lavash, different varieties of adorable grapes and divine apricot. You can't even imagine what indescribable feelings you will have during your visit of cognac factories and wineries of Armenia.
+  </p>
+  <p>
+    During the trip you will visit not only historical, architectural and spiritual centers of Armenia, which show the development of human civilization, but also you can see with your own eyes unique and colorful life of the Armenian people.
+  </p>
+  <p>
+    With us you will see ancient buildings and modern constructions, get acquainted with traditional Armenian rituals and today’s customs, you’ll be amazed and enjoy them at the same time, and return to your homeland – summarizing the beautiful sceneries of life.
   </p>
 `;

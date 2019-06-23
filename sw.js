3a12b2ae81e4ae6e3484e45569f4023383512eca
Arthur Khlghatyan <arthur.khlghatyan@gmail.com>
2019-06-23 13:00:59 +0400

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e0faee5c528d14e45983.js"
+    "url": "webpack-runtime-7110e1f645d25d1caf9e.js"
   },
   {
     "url": "app-fc1c4aabf9813bac4cc1.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "921b9bdab0d120a853919044a61783ec"
+    "revision": "659714ce7202001375c74a1f26ed9300"
   },
   {
     "url": "styles.f37ac972a6bd8a0750af.css"
